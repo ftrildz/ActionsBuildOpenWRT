c870e2bdb76fb2d3ff268e476efa5acc3ed94ce1
camino13 <54771820+camino13@users.noreply.github.com>
2021-02-09 12:20:41 +0800

    remove feed source

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,3 @@ sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' ./feeds.conf.default
-sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' ./feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
