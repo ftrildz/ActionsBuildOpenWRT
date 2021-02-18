7b04782713eb12cea638cccccd80f1ac150c15d4
ftfkdz66 <54771820+ftfkdz66@users.noreply.github.com>
2021-02-18 22:45:49 +0800

    add openclash apps

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,20 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # ==========luci-app-==========
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-OpenClash
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # ==========luci-theme-==========
 cd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+# ==========luci-app-openclash==========
+cd ../..
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
